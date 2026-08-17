610db2883d56e84adc5ccde9109e15ec6d26f009
HoangVu-startwork <hoangvu.startwork@gmail.com>
2026-08-17 15:50:23 +0700

    Tìm số lớn nhất trong mảng có sẳn

---
+++ algorithms/findMax.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+function findMax(arr) {
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+module.exports = findMax;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const findMax = require("./algorithms/findMax")
+
+
+const arr = [10, 5, 20, 8, 30, 15];
+
+const result = findMax(arr);
+
+console.log("Số lớn nhất:", result);
